0443f1d83ed858da594df7e7e765b1c8db968ee6
kethesainikhil <nikhilkethe007@gmail.com>
2024-02-22 23:33:35 +0530

    allowed all req

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ database.once('connected', () => {
 const app = express();
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "*",
     }
 ));
 app.use(express.json());
